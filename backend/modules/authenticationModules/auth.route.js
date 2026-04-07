90ed3437482a81a5ce1fefcdd4ea7cef21277203
quan-du <s4099918@rmit.edu.vn>
2026-04-07 14:54:43 +0700

    Wrote validation middlewares for authentication

---
+++ backend/modules/authenticationModules/auth.route.js
@@ -0,0 +1,12 @@
+// Importing dependencies: 
+import {Router} from 'express';
+const AuthRouter = Router();
+
+// Signing up:  
+AuthRouter.post('/signup',)
+
+// Logging in:
+AuthRouter.get('/login', )
+
+// Export
+export default AuthRouter;
