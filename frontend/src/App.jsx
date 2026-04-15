b4c9ec35a7720b83d36abc907fe7be40556f8974
SuperP3ter <78636680+SuperP3ter@users.noreply.github.com>
2026-04-16 01:22:17 +0700

    Made some resuable components and sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
+// import './App.css'
 
 function App() {
   <h1>Hello</h1>
---
+++ frontend/src/assets/UserBox.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+// need link update
+import defaultAvatar from ''
+
+export default function UserBox({username, avatarUrl}) {
+    return (
+        <Link to="/profile" 
+              className="flex w-full items-center gap-3 bg-[#0a1b4d] p-4">
+            
+            <div className="w-[25%] max-w-12 aspect-square shrink-0 rounded-full border-2 border-white/20 bg-slate-700">
+                <img src={avatarUrl || defaultAvatar} 
+                     alt="Profile-Img" 
+                     className="h-full w-full object-cover" 
+                />
+            </div>
+
+            <div className="flex-1 min-w-0 flex-col">
+                <div className="truncate text-base font-bold text-white font-baloo">
+                    {username}
+                </div>
+                <div className="text-[10px] tracking-wider text-gray-300 uppercase"> 
+                    Click to see profile 
+                </div>
+            </div>
+        </Link>
+    )  
+}
