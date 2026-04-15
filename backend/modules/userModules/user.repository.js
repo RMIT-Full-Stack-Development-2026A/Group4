ed135b704319ef036b73253983511ab0553b7d6a
Nguyen Hoang Son <s3990627@rmit.edu.vn>
2026-04-15 15:45:02 +0700

    refine userModel and userRepo

--- backend/modules/userModules/user.repository.js
+++ backend/modules/userModules/user.repository.js
@@ -1,16 +1,21 @@
 
-export const createUser = () => {
+import User from "./user.model";
 
+export const createUser = async (userData) => {
+    return await User.create(userData);
 };
-export const findUserBasedOnId = () => {
 
+export const findUserBasedOnId = async (userId) => {
+    return await User.findById(userId);
 };
-export const findUserBasedOnemail = () => {
 
-}
-export const deleteUser = () => {
-
+export const findUserBasedOnEmail = async(email) => {
+    return await User.findOne({email});
 };
-export const updateUser = () => {
 
+export const deleteUser = async (userId) => {
+    return await User.findByIdAndDelete(userId);
+};
+export const updateUser = async(userId, updateData) => {
+    return await User.findByIdAndUpdate(userId, updateData, { new: true });
 }
