1f36812154063122afd0e804b6e47b76ef137716
SuperP3ter <78636680+SuperP3ter@users.noreply.github.com>
2026-04-26 15:45:06 +0700

    some small changes for admin frontend

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,13 @@ app.use(express.json()) // Enable json formatting
 app.use('/', IndexRouter);
 app.use('/subscription', SubscriptionRoute );
 app.use('/auth', AuthRouter);
-app.use('/admin', AdminRouter)
+app.use('/admin', AdminRouter);
+
+// invalid routes
+app.all('*', (req, res, next) => {
+    next(new AppError(404, `The route ${req.originalUrl} does not exist on this server.`)); 
+});
+
 // Error Handler
 app.use(ErrorHandler);
 // Listening on port: 
--- backend/modules/adminModules/admin.route.js
+++ backend/modules/adminModules/admin.route.js
@@ -6,8 +6,10 @@ import { isAdmin, validateToggleInput } from './admin.middleware';
 
 const AdminRouter = Router();
   
+AdminRouter.use(isAdmin)
 // get all players
-AdminRouter.get('/players', isAdmin, AdminController.getAllPlayers)
+AdminRouter.get('/players', AdminController.getAllPlayers)
 // activate/deactivate player
-AdminRouter.post('/players/:id', isAdmin, validateToggleInput, AdminController.toggleAccountStatus)
+AdminRouter.post('/players/:id', validateToggleInput, AdminController.toggleAccountStatus)
+
 export default AdminRouter;
