bf89924157a24f2199b0028c844b2c98806d986f
Lam Quan Du <kobewannkenobi@Lams-MacBook-Air.local>
2026-05-01 21:14:04 +0700

    Sidebar generates based on user role and renders dynamically

--- frontend/src/config/NavConfig.js
+++ frontend/src/config/NavConfig.js
@@ -1,16 +1,35 @@
+import { Sword, LayoutDashboard, Gamepad, Edit, User, ClipboardCheck, History } from "lucide-react";
 
 const NavConfig = {
     "PLAYER": [
-        { type: "link", label: "", path: ""}
+        {
+            label: 'GAME',
+            links: [
+                { to: '/lobby', text:'START', icon: Sword },
+                { to: '/statistics', text:'STATISTICS', icon: LayoutDashboard },
+                { to: '/history', text:'HISTORY', icon: Gamepad },
+            ]
+        },
+        {
+            label: 'PROFILE',
+            links: [
+                { to: '/profile', text: 'VIEW INFO', icon: User  },
+                { to: '/profile/edit', text:'EDIT', icon: Edit  },
+            ]
+        },
+        {
+            label: 'SUBSCRIPTION',
+            links: [
+                { to: '/subscription', text:'PLANS', icon:ClipboardCheck },
+                { to: '/subscription/history', text:'HISTORY', icon: History},
+            ]
+        }
     ],
     "ADMIN": [
         { 
-            type: "section",
-            title: "Admin Functions",
-            children: [
-                { type: "link", label: "Player Management", path: ""}
-            ]
-        }
+            label: "section",
+            links: [],
+        },
     ]
 }
 
--- frontend/src/modules/lobby/components/SelectBoardLayout.jsx
+++ frontend/src/modules/lobby/components/SelectBoardLayout.jsx
@@ -18,7 +18,8 @@ const SelectBoardLayout = () => {
   // Submit Chosen Board Layout:
   // Carousel layout With board in middle and arrow pointing forward and backward to toggle
   return (
-    <div className=' min-h-screen bg-gray-100 flex flex-col justify-evenly '>
+    <div className=' min-h-screen shadow-[0px_0px_6px_1px_rgba(0,_0,_0,_0.1)] p-2 flex flex-col justify-evenly '>
+        
         <div className='flex items-center justify-center gap-2 align-middle'>
           <h1 className='text-3xl font-semibold'>Select Board Layout</h1>
           <Grid3x3 className='sm:hidden md:block' size={60} />
@@ -48,9 +49,9 @@ const SelectBoardLayout = () => {
           </button>
         </div>
         
-        <div className='flex gap-5 sm:flex-col md:flex-row '>
-          <button className='flex-1/2 border text-3xl font-bold p-5 rounded-lg transition-transform cursor-pointer hover:scale-101 duration-200' onClick={()=>{ setCurrentLayout(10);}}>10x10</button>
-          <button className='flex-1/2 border text-3xl font-bold p-5 rounded-lg transition-transform cursor-pointer hover:scale-101 duration-200' onClick={()=>{ setCurrentLayout(15);}}>15x15</button>
+        <div className='flex gap-5 flex-col justify-center align-middle items-center md:flex-row '>
+          <button className='flex-1/3 border text-xl font-bold p-3 rounded-lg transition-transform cursor-pointer hover:scale-101 duration-200' onClick={()=>{ setCurrentLayout(10);}}>10x10</button>
+          <button className='flex-1/3 border text-xl font-bold p-3 rounded-lg transition-transform cursor-pointer hover:scale-101 duration-200' onClick={()=>{ setCurrentLayout(15);}}>15x15</button>
         </div>
     
     </div>
