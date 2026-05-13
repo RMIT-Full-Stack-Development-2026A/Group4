daf6fc287e07df40ecba226b0e380dd745e1bfed
SuperP3ter <78636680+SuperP3ter@users.noreply.github.com>
2026-05-13 23:14:18 +0700

    forgot to import useSignup

--- frontend/src/modules/authentication/pages/Signup.jsx
+++ frontend/src/modules/authentication/pages/Signup.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../context/UserContext'
+import useSignup from '../hooks/useSignup'
 
 const Signup = () => {
     const { 
