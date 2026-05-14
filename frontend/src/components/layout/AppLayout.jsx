7430a8c0274c9468f0dd6d6d835fe178e6cf767c
SuperP3ter <78636680+SuperP3ter@users.noreply.github.com>
2026-05-14 21:55:56 +0700

    separate public and private sections

--- frontend/src/components/layout/AppLayout.jsx
+++ frontend/src/components/layout/AppLayout.jsx
@@ -1,18 +1,26 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '../../context/UserContext';
+import SideBar from '../reusable/SideBar';
 
 export default function AppLayout() {
     const { user, loading } = useAuth();
 
-    if (loading) {
-        return <div className='p-10 font-bold text-center'>Authenticating...</div>;
-    }
+    // Wait for security check
+    if (loading) return <div className='p-10 font-bold text-center uppercase tracking-widest'>Authenticating...</div>;
 
-    // If there is no user, redirect them to the landing page (Root)
+    // If not authenticated, user is redirected to login page
     if (!user) {
-        return <Navigate to="/" replace />;
+        return <Navigate to="/login" replace />;
     }
 
-    // If the user IS logged in, allow them to see the page
-    return <Outlet />;
+    return (
+        <div className='flex flex-row h-screen overflow-hidden bg-white'>
+            <SideBar />
+            
+            <main className='flex-1 overflow-y-auto p-3'>
+              <Outlet/>
+            </main>
+
+        </div>
+    );
 };
